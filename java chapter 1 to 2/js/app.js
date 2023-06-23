2546623a4cf2f9ef543b827b646135a549bb73fe
Areeshaywaheed <128454186+Areeshaywaheed@users.noreply.github.com>
2023-06-23 22:02:25 +0500

    Add files via upload

---
+++ java chapter 1 to 2/js/app.js
@@ -0,0 +1,89 @@
+                  //========== "1st chapter started" ==========
+
+//==========================Q1=====================================
+// alert("Hi everyone! Welcome to my Website!");
+
+//==========================Q2=====================================
+// alert("Error! Please enter a valid password.");
+
+//==========================Q3=====================================
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+//==========================Q4=====================================
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+//==========================Q5=====================================
+//Type this in Console
+// alert("Hello... I can run JS through my web browser's console");
+
+//==========================Q6=====================================
+//Done
+
+//==========================Q7=====================================
+//Checked
+
+                        //========== "1st chapter completed" ==========
+
+
+
+                       //============ "2nd chapter started" ============
+
+//==========================Q1=====================================
+// var userName = "Areesha";
+//Check the output in console
+
+//==========================Q2======================================
+// var myName = "Areesha Abdul Waheed";
+//Check the output in console
+
+//==========================Q3=====================================
+// var message = "Hello World";
+// alert(message);
+
+//==========================Q4=====================================
+// let name = prompt("Enter Your Name");
+// alert(name);
+// let age = +prompt("Enter your age");
+// alert(age);
+// let qualified = prompt("Certified or Not??");
+// alert(qualified + " Mobile Application Development");
+
+//==========================Q5=====================================
+// let food = "PIZZA";
+// console.log(food);
+
+// let final = food.slice(0, food.length-1);
+// console.log(final);
+
+// final = food.slice(0, food.length-2);
+// console.log(final);
+
+// final = food.slice(0, food.length-3);
+// console.log(final);
+
+// final = food.slice(0, food.length-4);
+// console.log(final);
+
+// final = food.slice(0, food.length-5);
+// console.log(final);
+
+//==========================Q6=====================================
+// let email = "areeshawaheed98@gmail.com";
+// alert(email);
+
+//==========================Q7=====================================
+// let book = "A Smarter Way to Learn JavaScript.";
+// alert("I am trying to learn from the Book " + book); 
+
+//==========================Q8=====================================
+// let js = "Yah! I can write HTML content through JavaScript";
+// document.write(js);
+
+//==========================Q9=====================================
+// let symbol = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(symbol)
+
+
+
+                      //============ "2nd chapter completed" ============
